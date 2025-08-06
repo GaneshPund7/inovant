70169a088cd640d42c5ba502339c426ec1da4b1a
Ganeshpund7 <ganeshpund0000@gmail.com>
2025-08-07 00:07:13 +0530

    added db url

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('sequelize');
  
-const sequelize = new Sequelize('inovant_store', 'postgres','Ganesh', {
-  host: 'localhost', 
+const sequelize = new Sequelize('postgresql://inovant_user:95fhYIox7X7nnjP3ByKYC1meQ75N7ztM@dpg-d29pueemcj7s73ckkfh0-a.oregon-postgres.render.com/inovant', {
   dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false, 
 });
+
 sequelize.authenticate()
-  .then(() => console.log('Database connected successfully.'))
+  .then(() => console.log('Database connected successfully to Render PostgreSQL.'))
   .catch(err => console.error('Connection failed:', err));
 
-  module.exports = sequelize
+module.exports = sequelize;
