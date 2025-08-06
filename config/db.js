e73f3323a366be486b78644df1508208dc94af1f
Ganeshpund7 <ganeshpund0000@gmail.com>
2025-08-07 00:10:34 +0530

    changes in db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,20 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
- 
-const sequelize = new Sequelize('postgresql://inovant_user:95fhYIox7X7nnjP3ByKYC1meQ75N7ztM@dpg-d29pueemcj7s73ckkfh0-a.oregon-postgres.render.com/inovant', {
+
+const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  logging: false, 
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,  
+    },
+  },
+  logging: false,
 });
 
 sequelize.authenticate()
-  .then(() => console.log('Database connected successfully to Render PostgreSQL.'))
-  .catch(err => console.error('Connection failed:', err));
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error(' Connection failed:', err));
 
 module.exports = sequelize;
